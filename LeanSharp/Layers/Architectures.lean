66035bb2a7c4ca0c269a5951644a1a0694ded923
Bangyen Pham <bangyen99@gmail.com>
2026-04-05 16:45:38 -0500

    Formalize permutation equivariance of Multi-Head Attention

--- LeanSharp/Layers/Architectures.lean
+++ LeanSharp/Layers/Architectures.lean
@@ -5,6 +5,7 @@ Authors: Bangyen Pham
 -/
 
 import LeanSharp.Layers.Architectures.Attention
+import LeanSharp.Layers.Architectures.AttentionInvariance
 import LeanSharp.Layers.Architectures.Transformer
 import LeanSharp.Layers.Architectures.VisionTransformer
 
